e1195e2c6cdc5a376417ce6ebe518ead9addf2f3
Bohemian <153691783+StarHorizonsSA@users.noreply.github.com>
2024-08-11 10:21:41 -0700

    Add files via upload

--- main-scripts.js
+++ main-scripts.js
@@ -10,15 +10,22 @@ window.addEventListener('load', () => {
         console.log("Logo element found:", logo);
         setTimeout(() => {
             console.log("Applying hidden class to logo");
-            logo.classList.add('hidden');
+            logo.classList.add('hidden'); // Start the fade-out animation
+    
+            // Wait for the fade-out animation to complete
+            setTimeout(() => {
+                logo.style.display = 'none'; // Remove the logo from the document flow
+                const container = document.querySelector('.container');
+                container.style.height = 'auto'; // Adjust the container's height
+            }, 2000); // Match this timeout with the duration of the fadeOut animation
+    
             mainHeading.classList.remove('hidden-heading');
             subHeading.classList.remove('hidden-heading');
-            console.log("Current logo classes:", logo.className);
-            console.log("Current logo style opacity:", window.getComputedStyle(logo).opacity);
-        }, 1000); // Wait 1 seconds before adding the 'hidden' class
+        }, 2000); // Wait 2 seconds before adding the 'hidden' class
     } else {
         console.log("Logo element not found");
     }
+    
 });
 
 // FUNCTIONS AND LOGIC TO HANDLE PIP NUMBERS & USER INPUT
@@ -33,7 +40,8 @@ function viewPIP() {
     loadingModal.style.display = 'block';
 
     // Find the largest PIP number
-    findLargestPipNumber(1000, function(largestPipNumber) {
+    findLargestPipNumber(10, function(largestPipNumber) {     //  <<-- CHANGE AS PIP FILES INCREASE
+        
         // Once the largest PIP number is found, hide the loading modal and show the input modal
         loadingModal.style.display = 'none';
         pipPromptModal.style.display = 'block';
@@ -68,7 +76,7 @@ function viewPIP() {
     });
 }
 
-function findLargestPipNumber(maxCheck = 1000, callback) {
+function findLargestPipNumber(maxCheck = 1000, callback) {   // Default value of 1000 if no argument is passed to function
     let largestPipNumber = 0;
     let currentCheck = 1;
 
@@ -103,24 +111,3 @@ function checkFileExists(url, callback) {
     };
     xhr.send();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
